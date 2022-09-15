6e8db6545bae8974feb9886eea66c16f9968fb50
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-09-15 11:01:08 -0300

    adding card

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,7 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles'
 import { 
 	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
 	Container, 
+	Grid, 
 	Typography,
 } from '@material-ui/core'
 
@@ -10,6 +15,9 @@ import TemplateDefault from '../../src/templates/Default'
 const useStyles = makeStyles((theme) => ({
 	container: {
 		padding: theme.spacing(8, 0, 6,)
+	},
+	cardMedia: {
+		paddingTop: '60%'
 	},
 	buttonAd: {
 		margin: '30px auto',
@@ -33,6 +41,111 @@ const Home = () => {
 				>
 					Post new ad
 				</Button>
+			</Container>
+
+			<Container maxWidth='md'>
+				<Grid container spacing={4}>
+					<Grid item xs={12} sm={6} md={4}>
+						<Card>
+							<CardMedia  
+								className={classes.cardMedia}
+								image={'https://source.unsplash.com/random'}
+								title='Image title'
+							/>
+							<CardContent>
+								<Typography variant='h5' component='h2'>
+									Product x
+								</Typography>
+								<Typography>
+									$ 20,00
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button size='small' color='primary'>
+									Edit
+								</Button>
+								<Button size='small' color='primary'>
+									Remove
+								</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					<Grid item xs={12} sm={6} md={4}>
+						<Card>
+							<CardMedia  
+								className={classes.cardMedia}
+								image={'https://source.unsplash.com/random'}
+								title='Image title'
+							/>
+							<CardContent>
+								<Typography variant='h5' component='h2'>
+									Product x
+								</Typography>
+								<Typography>
+									$ 20,00
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button size='small' color='primary'>
+									Edit
+								</Button>
+								<Button size='small' color='primary'>
+									Remove
+								</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					<Grid item xs={12} sm={6} md={4}>
+						<Card>
+							<CardMedia  
+								className={classes.cardMedia}
+								image={'https://source.unsplash.com/random'}
+								title='Image title'
+							/>
+							<CardContent>
+								<Typography variant='h5' component='h2'>
+									Product x
+								</Typography>
+								<Typography>
+									$ 20,00
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button size='small' color='primary'>
+									Edit
+								</Button>
+								<Button size='small' color='primary'>
+									Remove
+								</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					<Grid item xs={12} sm={6} md={4}>
+						<Card>
+							<CardMedia  
+								className={classes.cardMedia}
+								image={'https://source.unsplash.com/random'}
+								title='Image title'
+							/>
+							<CardContent>
+								<Typography variant='h5' component='h2'>
+									Product x
+								</Typography>
+								<Typography>
+									$ 20,00
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button size='small' color='primary'>
+									Edit
+								</Button>
+								<Button size='small' color='primary'>
+									Remove
+								</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+				</Grid>
 			</Container>
 		</TemplateDefault>
 	)
