ae40ccc20db119c56920b1e7237b575457e16a67
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-10-20 10:38:26 -0300

    remove image

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -80,7 +80,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Publish = () => {
    const classes = useStyles()
-   const [files, setFiles] = useState([])
+   const [ files, setFiles ] = useState([])
 
    const { getRootProps, getInputProps } = useDropzone({
       accept: 'image/*',
@@ -97,6 +97,11 @@ const Publish = () => {
          ])
       }
    })
+
+   const handleRemoveFile = fileName => {
+      const newFileState = files.filter(file => file.name !== fileName)
+      setFiles(newFileState)
+   }
 
    return (
       <TemplateDefault>
@@ -215,7 +220,10 @@ const Publish = () => {
                            }
 
                            <Box className={classes.mask}>
-                              <IconButton color='secondary'>
+                              <IconButton
+                                 color='secondary'
+                                 onClick={() => handleRemoveFile(file.name)}
+                              >
                                  <DeleteForever fontSize="large" />
                               </IconButton>
                            </Box>
